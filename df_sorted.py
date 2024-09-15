392c7d5139f98c8ac48767e67bd54b1ea8b97c41
renzisen <xj888xj@qq.com>
2024-09-15 16:38:28 +0800

    Update df_sorted.py

--- df_sorted.py
+++ df_sorted.py
@@ -56,20 +56,6 @@ df_gj = df_sorted[df_sorted['国家'] == 'SG']
 # 将排序后的DataFrame保存回CSV文件
 df_gj.to_csv('latest_sorted-SG.csv', index=False)
 #####################################################################################
-## 读取CSV文件
-#df = pd.read_csv('latest.csv')
-#
-## 创建一个新的列，其中包含"峰值速度"列的值，但是已经去掉了单位（kB/s）
-#df['峰值速度数值'] = df['峰值速度'].str.replace(' kB/s', '').astype(float)
-#
-## 按照"峰值速度数值"列的值进行排序
-#df_sorted = df.sort_values(by='峰值速度数值', ascending=False)
-#
-#df_gj = df_sorted[df_sorted['国家'] == 'JP']
-#
-## 将排序后的DataFrame保存回CSV文件
-#df_gj.to_csv('latest_sorte-JP.csv', index=False)
-#####################################################################################
 # 读取CSV文件
 df = pd.read_csv('latest.csv')
 
